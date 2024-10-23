e6d2bee52610daa58851874a5d06b14ab18d7f9d
yahia <y_kilany@aucegypt.edu>
2024-10-24 00:03:35 +0300

    added a placeholder driver code

---
+++ Src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Gates.h"
+#include "Circuit.h"
+#include "ParseVerilog.h"
+#include "ParseStim.h"
+
+int main () {
+    ParseVerilog parser;
+    Circuit circuit = parser.parse ("./Tests/parity_checker.v");
+    circuit.simulate ();
+    circuit.printValues ();
+
+    std::vector<std::map<std::string , int>> stimuli = parseStimFile ("./Tests/stimulus.txt");
+    for (const auto& stimulus : stimuli) {
+        for (const auto& [name , value] : stimulus) {
+            std::cout << name << ": " << value << std::endl;
+        }
+    }
+
+    return 0;
+}
