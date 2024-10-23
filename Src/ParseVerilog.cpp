3fabcd49345a4c788fd31419a2650683a3135304
yahia <y_kilany@aucegypt.edu>
2024-10-24 00:03:07 +0300

    Moved function Remove characters

--- Src/ParseVerilog.cpp
+++ Src/ParseVerilog.cpp
@@ -1,4 +1,14 @@
 #include "ParseVerilog.h"
+std::string removeCharacters (const std::string& input , const std::string& charsToRemove) {
+    std::string result = input; // Copy the input string to result
+
+    // Use std::remove_if and erase to remove unwanted characters
+    result.erase (std::remove_if (result.begin () , result.end () ,
+        [&](char c) { return charsToRemove.find (c) != std::string::npos; }) ,
+        result.end ());
+
+    return result; // Return the modified string
+}
 Circuit ParseVerilog::parse (const std::string& filename) {
     const std::string charsToRemove = "(),; ";
 
--- Src/ParseVerilog.h
+++ Src/ParseVerilog.h
@@ -10,16 +10,7 @@
 #ifndef PARSEVERILOG_H
 #define PARSEVERILOG_H
 using namespace std;
-std::string removeCharacters (const std::string& input , const std::string& charsToRemove) {
-    std::string result = input; // Copy the input string to result
 
-    // Use std::remove_if and erase to remove unwanted characters
-    result.erase (std::remove_if (result.begin () , result.end () ,
-        [&](char c) { return charsToRemove.find (c) != std::string::npos; }) ,
-        result.end ());
-
-    return result; // Return the modified string
-}
 
 class ParseVerilog {
 public:
