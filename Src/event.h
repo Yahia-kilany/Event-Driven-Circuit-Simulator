c81b16f66239a54d2752252b4e4f9552d422a13d
yahia <y_kilany@aucegypt.edu>
2024-10-27 04:16:10 +0300

    created the event class

---
+++ Src/event.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+class event {
+private:
+    int time;
+    std::string name;
+    int value;
+    
+    // Declare CompareEvent and the output operator as friends for access to private members
+    friend std::ostream& operator<<(std::ostream& os, const event& e);
+    friend struct CompareEvent;
+
+public:
+    // Constructor using move semantics for the string
+    event(int t, std::string str, int val) : time(t), name(std::move(str)), value(val) {}
+
+    // Getters
+    int getValue() const { return value; }
+    int getTime() const { return time; }
+    const std::string& getName() const { return name; }
+};
+
+// Overloaded output operator for event
+std::ostream& operator<<(std::ostream& os, const event& e) {
+    os << e.time << " " << e.name << " " << e.value;
+    return os;
+}
+
+// Comparison struct for event
+struct CompareEvent {
+    bool operator()(const event &e1, const event &e2) const {
+        return e1.time > e2.time; // For min-heap
+    }
+};
+
+int main() {
+    event e1(10, "Sample Event", 100);
+    std::cout << e1 << std::endl; // Output: 10 Sample Event 100
+
+    return 0;
+}
